b9ba9989da3d38764e9bf2ed0c60bc409241d8a5
realrgt <ergitorubildo@gmail.com>
2020-03-03 10:20:40 +0200

    äppbar

--- lib/home.dart
+++ lib/home.dart
@@ -8,6 +8,46 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(
+          color: Colors.grey,
+          opacity: 1
+        ),
+        title: Image.asset(
+          "images/youtube.png",
+          width: 98,
+          height: 22,
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.videocam
+            ),
+            onPressed: (){
+              print("Video action");
+            },
+          ),
+          IconButton(
+            icon: Icon(
+                Icons.search
+            ),
+            onPressed: (){
+              print("Search action");
+            },
+          ),
+          IconButton(
+            icon: Icon(
+                Icons.account_circle
+            ),
+            onPressed: (){
+              print("Account action");
+            },
+          )
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
